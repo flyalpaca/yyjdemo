287a58347b4a0c5036ac7a49f11bc360d0ff1ac7
有问号的生物 <yyj0519@outlook.com>
2021-02-07 11:36:28 +0800

    demo

---
+++ src/main/java/com/yyyy/test/yyj/main.java
@@ -0,0 +1,17 @@
+package com.yyyy.test.yyj;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("test插件已启动");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
